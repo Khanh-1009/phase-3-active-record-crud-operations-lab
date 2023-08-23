23877a40aa0af5822cc3f5519b0f6433959eda82
Khanh Tran <katetrannbkhanh@gmail.com>
2023-08-22 18:07:43 -0700

    commit tree de1e81167a2d51b407ce1b647aa89efc985a227f

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,45 @@
 class Movie < ActiveRecord::Base
+    def Movie.create_with_title (title)
+        self.create(title: title)
+    end
 
+    def self.first_movie
+        Movie.first
+    end
+
+    def Movie.last_movie
+        self.last 
+    end
+
+    def self.movie_count
+        Movie.count
+    end
+
+    def Movie.find_movie_with_id(id)
+        self.find(id)
+    end
+
+    def Movie.find_movie_with_attributes(attr)
+        Movie.find_by(attr)
+    end
+
+    def Movie.find_movies_after_2002
+        self.where("release_date > ?", 2002)
+    end
+
+    def update_with_attributes(attr)
+        self.update(attr)
+    end
+
+    def Movie.update_all_titles(title)
+        self.update_all(title: title)
+    end
+
+    def Movie.delete_by_id(id)
+        Movie.destroy(id)
+    end
+
+    def Movie.delete_all_movies
+        self.destroy_all
+    end
 end
